30b3c9afdfdff30644c9a5b3dc68523bad6cf6a8
hjp309 <hjp309@nyu.edu>
2016-09-23 00:22:22 -0400

    Add files via upload

---
+++ Week 2 - Alfredo Volpi 'Composição' code.js
@@ -0,0 +1,21 @@
+//Howard Park
+//Creative Coding
+//Week 2
+//Alfredo Volpi c. 1958 "Composição"
+
+function setup(){
+
+}
+
+function draw(){
+	createCanvas(500,500);
+	background('#F7E9C3');
+	
+	fill(color('#5E2929'));
+	beginShape();
+	vertex(0,500);
+	vertex(90,0);
+	vertex(210,120);
+	vertex(100,500);
+	endShape(CLOSE);
+}
