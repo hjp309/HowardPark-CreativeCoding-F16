1fdcd3fc8403bf2dba4ce41df1922d1d39b6236c
hjp309 <hjp309@nyu.edu>
2016-10-02 17:16:00 -0400

    Update Wall Drawing #17.js

--- Week-1/Wall Drawing #17.js
+++ Week-1/Wall Drawing #17.js
@@ -34,28 +34,28 @@ function draw(){
 	//Rectangle with bottom left to top right, diagonal lines
 	var totalLines = 20;
 	var dGap = rectWidth/totalLines;
-	for(dLinesTop = 0; dLinesTop < totalLines; dLinesTop++){
+	for(var dLinesTop = 0; dLinesTop < totalLines; dLinesTop++){
 		line(margin + rectWidth*2 + gap, margin + dLinesTop*dGap, margin + rectWidth*2 + gap + dLinesTop*dGap, margin);
 	}
 
-	for(dLinesMid = 0; dLinesMid < totalLines*3.5; dLinesMid++){
+	for(var dLinesMid = 0; dLinesMid < totalLines*3.5; dLinesMid++){
 		line(margin + rectWidth*2 + gap, margin + totalLines*dGap + dLinesMid*dGap, margin + gap + totalLines*3*dGap, margin + dLinesMid*dGap);
 	}
 
-	for(dLinesBot = 0; dLinesBot < totalLines; dLinesBot++){
+	for(var dLinesBot = 0; dLinesBot < totalLines; dLinesBot++){
 		line(margin + rectWidth*2 + gap + dLinesBot*dGap, margin + totalLines*4.5*dGap, margin + rectWidth*3 + gap, margin + totalLines*3.5*dGap + dLinesBot*dGap);
 	}
 
 	//Rectangle with bottom right to top left, diagonal lines
-	for(dLinesTop = 0; dLinesTop < totalLines; dLinesTop++){
+	for(var dLinesTop = 0; dLinesTop < totalLines; dLinesTop++){
 		line(width - margin - dLinesTop*dGap, margin, width - margin, margin + dLinesTop*dGap);
 	}
 
-	for(dLinesMid = 0; dLinesMid < totalLines*3.5; dLinesMid++){
+	for(var dLinesMid = 0; dLinesMid < totalLines*3.5; dLinesMid++){
 		line(width - margin, margin + totalLines*dGap + dLinesMid*dGap, width - rectWidth - margin, margin + dLinesMid*dGap);
 	}
 
-	for(dLinesBot = 0; dLinesBot < totalLines; dLinesBot++){
+	for(var dLinesBot = 0; dLinesBot < totalLines; dLinesBot++){
 		line(width - margin - dLinesBot*dGap, margin + totalLines*4.5*dGap, width - margin - rectWidth, margin + totalLines*3.5*dGap + dLinesBot*dGap);
 	}
 }
