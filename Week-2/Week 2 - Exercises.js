dcae10988dfc40dc6056a2accf1074e33b55162f
hjp309 <hjp309@nyu.edu>
2016-10-02 15:44:28 -0400

    Add files via upload

---
+++ Week-2/Week 2 - Exercises.js
@@ -0,0 +1,29 @@
+//Week 2 Exercises
+
+function setup(){
+
+}
+
+function draw(){
+	var posx = 100;
+	var posy = 50;
+	createCanvas(600,600);
+	background(0);
+
+	//Set var for rect position (x,y)
+
+	fill(color(255, 0, 0));
+	rect(posx,posy,100,100);
+
+	//Set square to mouse pos
+	rect(mouseX, mouseY, 100,100);
+
+	//Expression
+	rect(100, 100, rectwidth, 100);
+    rect(100, 300, rectwidth*3, 100);
+
+    //Set boundary for shape to appear
+	if(mouseX > 100 && mouseY > 100){
+        ellipse(mouseX, mouseY, 50, 50);
+    }
+}
