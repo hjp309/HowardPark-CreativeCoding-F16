0bdb7352fb2aee4e6399f03d6dd9d7cb1cd79e03
hjp309 <hjp309@nyu.edu>
2016-10-02 17:16:36 -0400

    Update Week 2 - Alfredo Volpi 'Composição' code.js

--- Week-2/Week 2 - Alfredo Volpi 'Composição' code.js
+++ Week-2/Week 2 - Alfredo Volpi 'Composição' code.js
@@ -1,18 +1,13 @@
-/**
-* Kevin's comments:  Make sure you call createCanvas inside setup, not draw.
-* Otherwise you will be creating an entirely new canvas each time draw is looped.
-*/
 //Howard Park
 //Creative Coding
 //Week 2
 //Alfredo Volpi c. 1958 "Composição"
 
 function setup(){
-
+	createCanvas(500,500);
 }
 
 function draw(){
-	createCanvas(500,500);
 	background('#F7E9C3');
 	
 	fill(color('#5E2929'));
