467f7d1aa773c587e3f148023e2931868e6517b8
hjp309 <hjp309@nyu.edu>
2016-10-14 01:45:31 -0400

    Add files via upload

---
+++ Week-4/Week 4 Homework.js
@@ -0,0 +1,25 @@
+//Howard Park
+//Creative Coding
+//Week 4 Homework
+
+function setup(){
+	createCanvas(windowWidth, windowHeight);
+	colorMode(HSB, 360, 100, 100);
+	background(45, 50, 100);
+}
+
+function draw(){
+	noStroke();
+	fill('#ff6666');
+	rect(0, windowHeight/2, windowWidth, windowHeight);
+	fill('#990000');
+	rect(0, windowHeight - 100, windowWidth, 100);
+	fill('#009933');h
+	rect(0, 0, windowWidth, windowHeight/2);
+	fill('#006600');
+	rect(0, 100, windowWidth, windowHeight/2 - 100);
+	fill('#993300');
+	rect(windowWidth/2 - 50, 50, 100, 100);
+	fill('#993300');
+	rect(windowWidth/2 - 50, windowHeight - 150, 100, 100);
+}
